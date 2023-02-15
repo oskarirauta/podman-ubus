16b451de785db804322f8f5ccd94a63b6ffe2934
Oskari Rauta <oskari.rauta@gmail.com>
2023-02-15 09:52:39 +0200

    default to libpod version 4.0.0
    Signed-off-by: Oskari Rauta <oskari.rauta@gmail.com>

--- podman/include/podman_constants.hpp
+++ podman/include/podman_constants.hpp
@@ -4,7 +4,7 @@ namespace Podman {
 
 	inline static const std::string API_SOCKET = "/run/podman/podman.sock";
 	inline static const std::string API_HOST = "localhost";
-	inline static const std::string API_VERSION = "v3.2.0";
+	inline static const std::string API_VERSION = "v4.0.0";
 	inline static const std::string API_PATH = "libpod";
 	inline static const std::string API_USERAGENT = "SystemBus";
 	inline static const std::string API_PROTOCOL = "HTTP/1.1";
